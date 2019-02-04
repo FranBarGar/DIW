72c688fa9eefd27eadaaeef3185872b5d00efbd5
FranciscoBarbaG <francisco.barba@iesdonana.org>
2019-02-04 04:04:46 +0100

    Ultima actividad

--- 2EV/2019.01.25 Actividad/js/script.js
+++ 2EV/2019.01.25 Actividad/js/script.js
@@ -1,11 +1,36 @@
 $(function(){
     loadImages();
 
+    /**
+     * Pone el div objetivo como droppable, siendo este el encargado de bloquear y encargarse
+     * @param  {Event} event Evento de drop.
+     * @param  {Event} ui    Evento que causa el drop (el draggable).
+     */
+    $('#ord').droppable({drop: function(event, ui){
+        var $carta = ui.draggable;
+        var $this = $(event.target);
+        var actual = $this.data('orden');
+        var id = $carta.prop('id');
+        if (!actual || actual<id) {
+            $carta.draggable({revert: false});
+            let $last = $(`img[id=${actual}]`)
+            $carta.css(
+                {
+                    'z-index': id,
+                    'left': `${parseInt($last.css('left'))+40}px`,
+                    'top': `${parseInt($last.css('top'))+40}px`
+                }
+            );
+            $this.data('orden', id);
+            $carta.draggable('destroy');
+        }
+    }});
 });
 
+/**
+ * Carga las imagenes en el navegador.
+ */
 function loadImages (){
-    var index=-100;
-
     var img = [
         'src/Barba Garcia, Francisco.jpg',
         'src/Barba Rodriguez, Arturo.jpg',
@@ -17,40 +42,44 @@ function loadImages (){
         'src/Vazquez Rodriguez, Maria del Mar.jpg',
     ];
 
+    setImgs(shuffle(initImgs(img)));
+}
+
+/**
+ * Inicializa el array de imagenes con los valores basicos iniciales.
+ * @param  {Array} ar Array a inicializar de imagenes.
+ * @return {Array}    Array ya inicializado.
+ */
+function initImgs(ar) {
     var $elem = [];
     var $aux;
-    var pos = 10;
-    for (var i = 0; i < img.length; i++) {
-        $aux = $(`<img src="${img[i]}">`);
-        $aux.data('index', i);
-        $aux.draggable();
-        $aux.on('drag', function(){
-            var $selector = $('#ord');
-            var actual = $selector.data('orden');
-            var id = $(this).data('index');
-            $(this).css('z-index', index);
-            index+=1;
-            if (actual && actual>id) {
-                $(this).draggable({revert: true});
-            } else {
-                $selector.data('orden', index);
-            }
-        });
+    for (var i = 0; i < ar.length; i++) {
+        $aux = $(`<img id="${i}" src="${ar[i]}">`);
+        $aux.draggable({revert: true});
         $elem.push($aux);
     }
-    var $elemDes = shuffle($elem);
+    return $elem;
+}
 
-    for (var i = 0; i < $elemDes.length; i++) {
-        $elemDes[i].css({'z-index': i, 'top': `${pos}px`, 'position': 'absolute'});
-        pos+=60;
-    }
-
+/**
+ * Pone las imagenes en su div correspondiente.
+ * @param {Array} ar Array de objetos a colocar.
+ */
+function setImgs(ar) {
     var $selector = $('#des');
-    for (var i = 0; i < $elemDes.length; i++) {
-        $selector.append($elemDes[i]);
+    var pos = 10;
+    for (var i = 0; i < ar.length; i++) {
+        ar[i].css({'z-index': i, 'top': `${pos}px`, 'position': 'absolute'});
+        pos+=60;
+        $selector.append(ar[i]);
     }
 }
 
+/**
+ * "Baraja" un array, haciendo que sea aleatorio
+ * @param  {Array} array Array a aleatorizar.
+ * @return {Array}       Array aleatorio.
+ */
 function shuffle(array) {
     var i = array.length, aux, random;
 
